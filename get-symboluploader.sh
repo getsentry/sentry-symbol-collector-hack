1362a28caf137fbeeef834b3729ef44194af052a
Armin Ronacher <armin.ronacher@active-4.com>
2017-04-12 15:12:39 -0700

    Added some prompts to the symbol uploader script

--- get-symboluploader.sh
+++ get-symboluploader.sh
@@ -15,6 +15,30 @@ if ! hash curl 2> /dev/null; then
   exit 1
 fi
 
+echo
+echo "This might take about 15 or 30 minutes depending on your CPU speed and how many"
+echo "SDKs you have on your Mac."
+echo
+
+while :; do
+  read -p "Do you want to continue? [yn] " -n 1 -r
+  if [ "x$REPLY" == "x" ]; then
+    break
+  fi
+  echo
+  case $REPLY in
+    [Yy])
+      break
+      ;;
+    [Nn])
+      exit
+      ;;
+    *)
+      echo "unknown input: type y or n to continue"
+      ;;
+  esac
+done
+
 TEMP_FILE=`mktemp "${TMPDIR:-/tmp}/.sentryuploader.XXXXXXXX"`
 
 cleanup() {
@@ -23,7 +47,7 @@ cleanup() {
 
 trap cleanup EXIT
 echo
-echo "➡️  Downloading..."
+echo "➡️  Downloading symbol extractor ..."
 curl -SL --progress-bar "$DOWNLOAD_URL" > "$TEMP_FILE"
 chmod +x "$TEMP_FILE"
 echo
